14100e796e6eb40da4b0d9eff98267597f671c5d
Cristina Llamas <cristina_roman@dennis.co.uk>
2018-01-25 19:55:59 +0000

    bits for netlity,https

--- js/projectstore.js
+++ js/projectstore.js
@@ -37,7 +37,7 @@ function ProjectStore(){
     /**
     *
     */
-    getJSON('http://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=chriskinch&api_key=8a1051b163165029d6dcaed68752152a&format=json',
+    getJSON('https://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=chriskinch&api_key=8a1051b163165029d6dcaed68752152a&format=json',
     function(err, data) {
       if (err != null) {
         alert('Something went wrong: ' + err);
